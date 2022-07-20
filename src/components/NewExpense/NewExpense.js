577836ed1d14f05f19cc907e2b09dd2152850b04
Adam Mackey <adambmackey@gmail.com>
2022-07-20 14:19:23 -0400

    added expense from

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './NewExpense'
+import ExpenseForm from './ExpenseForm'
+
+const NewExpense = () => {
+  return (
+    <div className='new-expense'>
+        <form>
+
+        </form>
+    </div>
+  )
+}
+
+export default NewExpense
