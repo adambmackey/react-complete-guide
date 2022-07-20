36a15bb4b9c9884d1188978a7fae583182ca5df5
Adam Mackey <adambmackey@gmail.com>
2022-07-20 13:07:29 -0400

    added more components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ExpenseDate from './ExpenseDate';
-import Card from './Card'
+import Card from '../UI/Card'
 import './ExpenseItem.css'; 
 
 const ExpenseItem = (props) => {
@@ -12,6 +12,7 @@ const ExpenseItem = (props) => {
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button>Change Title</button>
     </Card >
   );
 };
